c12a542abffbf80a0f86319ca16020ba54282bc8
KreedM <blackknight3377@gmail.com>
2024-11-11 04:04:25 -0800

    Pushed potential fix to cursor

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -7,24 +7,24 @@ var moving = false
 var lerp_tolerance = 1.0
 
 func _process(delta):
-    if moving:
-        move_towards_target(delta)
+	if moving:
+		move_towards_target(delta)
 
 func trace_route(direction: Vector2):
-    var tile_center = position.snapped(Vector2(32, 32)) + direction * 32
-    path.append(tile_center)
-    if not moving:
-        start_moving()
+	var tile_center = position.snapped(Vector2(32, 32)) + direction * 32
+	path.append(tile_center)
+	if not moving:
+		start_moving()
 
 func start_moving():
-    if path.size() > 0:
-        next_pos = path.pop_front()
-        moving = true
+	if path.size() > 0:
+		next_pos = path.pop_front()
+		moving = true
 
 func move_towards_target(delta):
-    position = position.lerp(next_pos, lerp_speed * delta)
-    if position.distance_to(next_pos) < lerp_tolerance:
-        position = next_pos
-        moving = false
-        # Transition to animation phase logic here
+	position = position.lerp(next_pos, lerp_speed * delta)
+	if position.distance_to(next_pos) < lerp_tolerance:
+		position = next_pos
+		moving = false
+		# Transition to animation phase logic here
 
--- scripts/TileMap.gd
+++ scripts/TileMap.gd
@@ -5,6 +5,7 @@ var mouse_pos : Vector2i = Vector2i(0,0)
 
 func _ready():
 	var layer_ind = _get_layer_index("Ground")
+
 	for cell in get_used_cells(layer_ind):
 		tile_data[cell] = Tile.new(Consts.TileType.Ground)
 
@@ -29,9 +30,19 @@ func _get_layer_index(layer_name: String) -> int:
 	return -1
 
 func _get_tile_pos(curr: Vector2i):
+	#var space_state = get_world_2d().direct_space_state
+	#var query = PhysicsPointQueryParameters2D.new()
+	#query.position = get_global_mouse_position()
+	#var result = space_state.intersect_point(query)
+	#if(result.size() > 0):
+	#	var tile_pos = get_coords_for_body_rid(result.front().rid)
+	#	if(tile_pos in tile_data):
+	#		return tile_pos
 	var tile_pos = local_to_map(get_local_mouse_position())
+	
 	if tile_pos in tile_data:
 		return tile_pos
+		
 	return curr
 
 func get_cursor_pos():
