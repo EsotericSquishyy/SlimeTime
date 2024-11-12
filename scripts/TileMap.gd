22c872c51f04d6d87384aa5330bda73b2df9b94a
EsotericSquishyy <jesselcobb@gmail.com>
2024-11-11 16:24:57 -0800

    Modified cursor, toggling overlay and slime layers/tiles

--- scripts/TileMap.gd
+++ scripts/TileMap.gd
@@ -1,48 +1,83 @@
 extends TileMap
 
 var tile_data = {}
-var mouse_pos : Vector2i = Vector2i(0,0)
+var cursor_pos : Vector2i = Vector2i(0,0)
+var over_hidden = false
+
+const TileType = Consts.TileType
 
 func _ready():
-    var layer_ind = _get_layer_index("Ground")
-
+    var layer_ind = get_layer_index("Ground")
     for cell in get_used_cells(layer_ind):
-        tile_data[cell] = Tile.new(Consts.TileType.Ground)
+        tile_data[cell] = TileVals.new(self, cell, layer_ind)
 
-    layer_ind = _get_layer_index("Water")
+    layer_ind = get_layer_index("Water")
     for cell in get_used_cells(layer_ind):
-        tile_data[cell] = Tile.new(Consts.TileType.Water)
+        tile_data[cell] = TileVals.new(self, cell, layer_ind)
 
-    # var coords = tile_data.keys().map(map_to_local).map(to_global)
-    # print(coords)
+    toggle_overlay()
+
+
 
 func _process(_delta):
-    mouse_pos = _get_tile_pos(mouse_pos)
+    cursor_pos = _get_tile_pos(cursor_pos)
 
-func update_tile(cell: Vector2, occupied: bool, effects: Array):
-    tile_data[cell]["occupied"] = occupied
-    tile_data[cell]["status_effects"] = effects
 
-func _get_layer_index(layer_name: String) -> int:
+
+func get_layer_index(layer_name: String) -> int:
     for i in range(get_layers_count()):
         if get_layer_name(i) == layer_name:
             return i
     return -1
 
+
+
 func _get_tile_pos(curr: Vector2i):
     var space_state = get_world_2d().direct_space_state
-    
     var query = PhysicsPointQueryParameters2D.new()
     query.collision_mask = Consts.TILE_OVERLAY_MASK
     query.position = get_global_mouse_position()
-    
-    var result = space_state.intersect_point(query)
-    if(result.size() == 1):
-        var tile_pos = get_coords_for_body_rid(result.front().rid)
-        if(tile_pos in tile_data):
-            return tile_pos
-        
-    return curr
+    var results = space_state.intersect_point(query)
+
+    var max_layer = -1
+    var temp_pos = curr
+    for result in results:
+        var tile_pos = get_coords_for_body_rid(result.rid)
+        var tile_layer = get_layer_for_body_rid(result.rid) 
+
+        if tile_layer > max_layer:
+            temp_pos = tile_pos
+            max_layer = tile_layer
+
+    return temp_pos
+
+    ### Alternate version [better :)]
+    # var tile_pos = local_to_map(get_local_mouse_position())
+    # if tile_pos in tile_data:
+    #     return tile_pos
+    # return curr
+
+
 
 func get_cursor_pos():
-    return to_global(map_to_local(mouse_pos))
+    return to_global(map_to_local(cursor_pos))
+
+
+
+func toggle_overlay():
+    var over_ind = get_layer_index("Overlay")
+    if over_hidden:
+        set_layer_modulate(over_ind, Color(1,1,1,1))
+        over_hidden = false
+    else:
+        set_layer_modulate(over_ind, Color(1,1,1,0))
+        over_hidden = true
+
+
+
+func _input(event):
+    if Input.is_action_just_pressed("ui_accept"):
+        toggle_overlay()
+
+    if event is InputEventMouseButton and event.pressed:
+            tile_data[cursor_pos].toggle_slime()
--- scripts/resources/TileData.gd
+++ scripts/resources/TileData.gd
@@ -1,14 +1,51 @@
 extends Resource
 
-class_name Tile
+class_name TileVals
 
 const TileType = Consts.TileType
-var _occupants: Array[Node2D]
+
+var _tileMap: TileMap
+var _pos: Vector2i
+var _layer: int
 var _slimed: bool
-var _type: TileType
+var _crossable: bool
 
-func _init(type: TileType = TileType.Ground, occupants: Array[Node2D] = [], slimed: bool = false):
-    self._type      = type
-    self._occupants = occupants
-    self._slimed    = slimed
+# var _occupant: Node2D
 
+func _init(tileMap: TileMap, pos: Vector2i, layer: int):
+    _tileMap   = tileMap
+    _pos       = pos
+    _layer     = layer
+
+    var tile_data = _tileMap.get_cell_tile_data(layer, self._pos)
+    init_slime(tile_data)
+    init_overlay(tile_data)
+
+
+
+func init_slime(tile_data: TileData):
+    _slimed = tile_data.get_custom_data("Slimed")
+    var slime_ind = _tileMap.get_layer_index("Slime")
+    if _slimed:
+        _tileMap.set_cell(slime_ind, _pos, 3, Vector2i(3,0))
+
+
+
+func init_overlay(tile_data: TileData):
+    _crossable = tile_data.get_custom_data("Crossable")
+    var over_ind = _tileMap.get_layer_index("Overlay")
+    if _crossable:
+        _tileMap.set_cell(over_ind, _pos, 3, Vector2i(2, 0))
+    else:
+        _tileMap.set_cell(over_ind, _pos, 3, Vector2i(1, 0))
+
+
+
+func toggle_slime():
+    var slime_ind = _tileMap.get_layer_index("Slime")
+    if _slimed:
+        _slimed = false
+        _tileMap.set_cell(slime_ind, _pos, -1)
+    else:
+        _slimed = true
+        _tileMap.set_cell(slime_ind, _pos, 3, Vector2i(3,0))
