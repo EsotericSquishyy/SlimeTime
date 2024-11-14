32443a85b7e1af3ba53c6fb02a788e0cc75400d9
KreedM <blackknight3377@gmail.com>
2024-11-13 18:20:28 -0800

    Moved constants to respective scripts

--- scripts/TileMap.gd
+++ scripts/TileMap.gd
@@ -1,16 +1,26 @@
 extends TileMap
 
-var tile_data = {}
-var cursor_pos : Vector2i = Vector2i(0,0)
+# Used for raycasting to get hovered tile
+const TILEMAP_COLLISION_LAYER = 1 << 7
 
-# Overlay variables
+# Used for offsetting cursor for half tiles
+const CURSOR_LOCAL_HALF_TILE_OFFSET = Vector2(0, 8)
+
+# Overlay layer names
+const TILEMAP_LAYER_SLIME = "Slime"
+const TILEMAP_LAYER_OVERLAY = "Overlay"
+const TILESET_SOURCE_OVERLAY = "Overlay"
+const TILESET_SOURCE_OVERLAY_HALF = "Overlay_Half"
+
+# Overlay vars
 var over_source_id = -1
 var over_half_source_id = -1
 var over_ind = -1
 var slime_ind = -1
 var over_hidden = false
 
-# const TileType = Consts.TileType
+var tile_data = {}
+var cursor_pos : Vector2i = Vector2i(0,0)
 
 func get_tileset_source_id(tileset_source_name: String) -> int:
     for i in range(tile_set.get_source_count()):
@@ -27,11 +37,11 @@ func get_layer_index(layer_name: String) -> int:
     return -1
 
 func _ready():
-    over_source_id = get_tileset_source_id(Consts.TILESET_SOURCE_OVERLAY)
-    over_half_source_id = get_tileset_source_id(Consts.TILESET_SOURCE_OVERLAY_HALF)
+    over_source_id = get_tileset_source_id(TILESET_SOURCE_OVERLAY)
+    over_half_source_id = get_tileset_source_id(TILESET_SOURCE_OVERLAY_HALF)
     
-    over_ind = get_layer_index(Consts.TILEMAP_LAYER_OVERLAY)
-    slime_ind = get_layer_index(Consts.TILEMAP_LAYER_SLIME)
+    over_ind = get_layer_index(TILEMAP_LAYER_OVERLAY)
+    slime_ind = get_layer_index(TILEMAP_LAYER_SLIME)
     
     for i in range(get_layers_count()): # Overlay and Slime layers should not have any cells initially
         for cell in get_used_cells(i):
--- scripts/resources/TileData.gd
+++ scripts/resources/TileData.gd
@@ -2,29 +2,36 @@ extends Resource
 
 class_name TileVals
 
-# const TileType = Consts.TileType
+# Overlay tileset atlas coords
+const TILESET_OVERLAY_UNCROSSABLE = Vector2i(1, 0)
+const TILESET_OVERLAY_CROSSABLE = Vector2i(2, 0)
+const TILESET_OVERLAY_SLIMED = Vector2i(3, 0)
+const TILESET_OVERLAY_SLIMED_TRANSPARENT = Vector2i(4, 0)
+const TILESET_OVERLAY_PATH = Vector2i(5, 0)
 
+# Custom data layer names
+const TILEDATA_SLIMED = "Slimed"
+const TILEDATA_CROSSABLE = "Crossable"
+const TILEDATA_IS_HALF_TILE = "Is_Half_Tile"
+
+# Vars for editing tilemap
 var _tileMap: TileMap
 var _pos: Vector2i
-
 var _over_source_id: int
 
-# var _layer: int
-
+# Tile data vars
 var _slimed: bool
 var _crossable: bool
 var _is_half_tile: bool
-
-# var _occupant: Node2D
 
 func _init(tileMap: TileMap, pos: Vector2i, layer: int):
     _tileMap   = tileMap
     _pos       = pos
 
     var tile_data = _tileMap.get_cell_tile_data(layer, _pos)
-    _crossable = tile_data.get_custom_data(Consts.TILEDATA_CROSSABLE)
-    _slimed = tile_data.get_custom_data(Consts.TILEDATA_SLIMED)
-    _is_half_tile = tile_data.get_custom_data(Consts.TILEDATA_IS_HALF_TILE)
+    _crossable = tile_data.get_custom_data(TILEDATA_CROSSABLE)
+    _slimed = tile_data.get_custom_data(TILEDATA_SLIMED)
+    _is_half_tile = tile_data.get_custom_data(TILEDATA_IS_HALF_TILE)
     
     #if(_is_half_tile):
         #_over_source_id = _tileMap.over_half_source_id
@@ -33,13 +40,13 @@ func _init(tileMap: TileMap, pos: Vector2i, layer: int):
 
 func update_overlay():
     if _crossable:
-        _tileMap.set_cell(_tileMap.over_ind, _pos, _over_source_id, Consts.TILESET_OVERLAY_CROSSABLE)
+        _tileMap.set_cell(_tileMap.over_ind, _pos, _over_source_id, TILESET_OVERLAY_CROSSABLE)
     else:
-        _tileMap.set_cell(_tileMap.over_ind, _pos, _over_source_id, Consts.TILESET_OVERLAY_UNCROSSABLE)
+        _tileMap.set_cell(_tileMap.over_ind, _pos, _over_source_id, TILESET_OVERLAY_UNCROSSABLE)
 
     if _slimed:
-        _tileMap.set_cell(_tileMap.over_ind, _pos, _over_source_id, Consts.TILESET_OVERLAY_SLIMED_TRANSPARENT)
-        _tileMap.set_cell(_tileMap.slime_ind, _pos, _over_source_id, Consts.TILESET_OVERLAY_SLIMED)
+        _tileMap.set_cell(_tileMap.over_ind, _pos, _over_source_id, TILESET_OVERLAY_SLIMED_TRANSPARENT)
+        _tileMap.set_cell(_tileMap.slime_ind, _pos, _over_source_id, TILESET_OVERLAY_SLIMED)
     else:
         _tileMap.set_cell(_tileMap.slime_ind, _pos, -1)
 
