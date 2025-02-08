f352d3225f1a11d64a00a9896b37461883d18f5b
KreedM <blackknight3377@gmail.com>
2025-02-07 18:39:53 -0800

    Heap

--- Scripts/Resources/Heap.gd
+++ Scripts/Resources/Heap.gd
@@ -2,7 +2,7 @@ extends RefCounted
 
 class_name Heap 
 
-var max : bool
+var max_heap : bool
 
 var num_entries : int
 
@@ -10,8 +10,8 @@ var entries : Array
 
 var indices : Dictionary
 
-func _init(_max : bool = false, _capacity : int = 4):
-    max = _max
+func _init(_max_heap : bool = false, _capacity : int = 32):
+    max_heap = _max_heap
         
     entries.resize(_capacity)
 
@@ -21,10 +21,10 @@ func top():
 func push(entry, value):
     if num_entries + 1 == entries.size():
         entries.resize(entries.size() << 1)
-        
-    indices[entry] = num_entries
     
     entries[num_entries] = [entry, value]
+    
+    indices[entry] = num_entries;
     
     _percolate_up(num_entries)
     
@@ -34,21 +34,25 @@ func pop():
     if is_empty():
         return
         
-    var top = top()
+    var entry = top()
     
     num_entries -= 1
     
-    indices.erase(entries[0])
-    indices[entries[num_entries]] = 0
+    indices.erase(top())
     
     entries[0] = entries[num_entries]
     entries[num_entries] = null
     
-    _percolate_down(0)
+    if not is_empty():
+        indices[top()] = 0
+        _percolate_down(0)
      
-    return top
+    return entry
     
 func modify(entry, value):
+    if not indices.has(entry):
+        return
+    
     var index = indices[entry]
     
     if _compare(value, entries[index][1]):
@@ -69,6 +73,8 @@ func is_empty():
 func clear():    
     entries.fill(null)
     
+    indices.clear()
+    
     num_entries = 0
     
 func _percolate_up(index : int):
@@ -85,7 +91,7 @@ func _percolate_up(index : int):
 func _percolate_down(index : int):
     while true:
         var new_index = (index << 1) + 2
-        
+
         if new_index < num_entries and _compare(entries[new_index][1], entries[new_index - 1][1]):
             pass
         elif new_index - 1 < num_entries:
@@ -101,7 +107,7 @@ func _percolate_down(index : int):
             break
     
 func _compare(val1, val2):
-    return max if (val1 > val2) else (val1 < val2)
+    return (val1 > val2) if max_heap else (val1 < val2)
     
 func _swap(index1 : int, index2 : int):
     var temp = entries[index1]
@@ -110,3 +116,7 @@ func _swap(index1 : int, index2 : int):
     
     indices[entries[index1]] = index1
     indices[entries[index2]] = index2
+
+func print_heap():
+    for i in range(num_entries):
+        print("[%s, %s]" % [entries[i][0], entries[i][1]])
